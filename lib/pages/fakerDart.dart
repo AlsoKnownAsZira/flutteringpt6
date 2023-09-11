45e88c40eb2b993ee9cbd39b59108cefe57d9280
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-09-11 23:36:29 +0700

    finished faker

--- lib/pages/fakerDart.dart
+++ lib/pages/fakerDart.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
 
 class fakerDart extends StatelessWidget {
-  const fakerDart({super.key});
+  var faker = new Faker();
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("faker"),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) => ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey[300],
+            backgroundImage: NetworkImage('https://loremflickr.com/${100+index}/320/240'),
+          ),
+          title: Text(faker.person.name()),
+          subtitle: Text(faker.internet.email()),
+        ),
       ),
     );
   }
